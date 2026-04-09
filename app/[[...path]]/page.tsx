2a81593941f380e7504f19f1e36fb21cd42b2d0c
illiakovalenko <illia.kovalenko@sitecore.com>
2026-04-09 09:46:19 +0300

    Test redirect

--- app/[[...path]]/page.tsx
+++ app/[[...path]]/page.tsx
@@ -29,5 +29,8 @@ export const generateStaticParams = async () => {
     {
       path: [],
     },
+    {
+      path: ["test"],
+    },
   ];
 };
--- app/api/render/route.ts
+++ app/api/render/route.ts
@@ -41,101 +41,101 @@ export const getQueryParamsForPropagation = (
   return params;
 };
 
-export async function GET(request: NextRequest) {
+export async function GET(request: Request) {
   console.log("Render request received");
-  try {
-    const draft = await draftMode();
+  // try {
+  const draft = await draftMode();
 
-    draft.enable();
+  draft.enable();
 
-    const route = request.nextUrl.searchParams.get("route") as string;
-    const requestUrl = new URL(route, resolveServerUrl(request));
+  // const route = request.nextUrl.searchParams.get("route") as string;
+  // const requestUrl = new URL(route, resolveServerUrl(request));
 
-    const cookieStore = await nextCookies();
-    const convertedCookies = cookieStore
-      .getAll()
-      .map((c: { name: string; value: string }) => `${c.name}=${c.value}`)
-      .join(";");
+  // const cookieStore = await nextCookies();
+  // const convertedCookies = cookieStore
+  //   .getAll()
+  //   .map((c: { name: string; value: string }) => `${c.name}=${c.value}`)
+  //   .join(";");
 
-    cookieStore.set(
-      PreviewCookies.PRERENDER_BYPASS,
-      cookieStore.get(PreviewCookies.PRERENDER_BYPASS)?.value || "",
-      {
-        httpOnly: true,
-        path: "/",
-        sameSite: "none",
-        secure: true,
-      }
-    );
+  // cookieStore.set(
+  //   PreviewCookies.PRERENDER_BYPASS,
+  //   cookieStore.get(PreviewCookies.PRERENDER_BYPASS)?.value || "",
+  //   {
+  //     httpOnly: true,
+  //     path: "/",
+  //     sameSite: "none",
+  //     secure: true,
+  //   }
+  // );
 
-    const propagatedQsParams = getQueryParamsForPropagation(
-      request.nextUrl.searchParams
-    );
+  // const propagatedQsParams = getQueryParamsForPropagation(
+  //   request.nextUrl.searchParams
+  // );
 
-    const propagatedHeaders: HeadersInit = {};
+  // const propagatedHeaders: HeadersInit = {};
 
-    // Get the page URL with the query params
+  // // Get the page URL with the query params
 
-    for (const key in propagatedQsParams) {
-      if (
-        {}.hasOwnProperty.call(propagatedQsParams, key) &&
-        propagatedQsParams[key]
-      ) {
-        requestUrl.searchParams.append(key, propagatedQsParams[key]);
-      }
-    }
+  // for (const key in propagatedQsParams) {
+  //   if (
+  //     {}.hasOwnProperty.call(propagatedQsParams, key) &&
+  //     propagatedQsParams[key]
+  //   ) {
+  //     requestUrl.searchParams.append(key, propagatedQsParams[key]);
+  //   }
+  // }
 
-    requestUrl.searchParams.append("route", route);
-    requestUrl.searchParams.append(
-      "item_id",
-      "{00000000-0000-0000-0000-000000000000}"
-    );
-    requestUrl.searchParams.append("language", "en");
-    requestUrl.searchParams.append("timestamp", Date.now().toString());
+  // requestUrl.searchParams.append("route", route);
+  // requestUrl.searchParams.append(
+  //   "item_id",
+  //   "{00000000-0000-0000-0000-000000000000}"
+  // );
+  // requestUrl.searchParams.append("language", "en");
+  // requestUrl.searchParams.append("timestamp", Date.now().toString());
 
-    // Grab the Next.js preview cookies to send on to the page render request
-    propagatedHeaders["cookie"] = convertedCookies;
+  // // Grab the Next.js preview cookies to send on to the page render request
+  // propagatedHeaders["cookie"] = convertedCookies;
 
-    console.log("Internal Request:", {
-      url: requestUrl.toString(),
-      headers: propagatedHeaders,
-    });
+  // console.log("Internal Request:", {
+  //   url: requestUrl.toString(),
+  //   headers: propagatedHeaders,
+  // });
 
-    const html = await fetch(requestUrl.toString(), {
-      credentials: "include",
-      headers: propagatedHeaders,
-      method: "GET",
-    })
-      .then((response) => {
-        console.log("Response: ", response);
-        return response.text();
-      })
-      .catch((error) => {
-        console.error("Error fetching page: ", error);
-        // We need to handle not found error provided by Vercel
-        // for `fallback: false` pages
-        if (error.response.status === 404) {
-          console.error("Page not found: ", error.response);
-          return error.response;
-        }
+  // const html = await fetch(requestUrl.toString(), {
+  //   credentials: "include",
+  //   headers: propagatedHeaders,
+  //   method: "GET",
+  // })
+  //   .then((response) => {
+  //     console.log("Response: ", response);
+  //     return response.text();
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error fetching page: ", error);
+  //     // We need to handle not found error provided by Vercel
+  //     // for `fallback: false` pages
+  //     if (error.response.status === 404) {
+  //       console.error("Page not found: ", error.response);
+  //       return error.response;
+  //     }
 
-        throw error;
-      });
+  //     throw error;
+  //   });
 
-    console.log("HTML: ", html);
+  // console.log("HTML: ", html);
 
-    if (!html || html.length === 0) {
-      throw new Error(`Failed to render html for ${requestUrl.toString()}`);
-    }
+  // if (!html || html.length === 0) {
+  //   throw new Error(`Failed to render html for ${requestUrl.toString()}`);
+  // }
 
-    console.log("Response headers:", {
-      "Content-Type": "text/html; charset=utf-8",
-      "Set-Cookie": convertedCookies,
-    });
+  // console.log("Response headers:", {
+  //   "Content-Type": "text/html; charset=utf-8",
+  //   "Set-Cookie": convertedCookies,
+  // });
 
-    redirect('/test');
-  } catch (error) {
-    console.error(error);
-    return new Response(null, { status: 500 });
-  }
+  redirect(`/test?test=true&language=en&timestamp=${Date.now()}`);
+  // } catch (error) {
+  // console.error(error);
+  // return new Response(null, { status: 500 });
+  // }
 }
